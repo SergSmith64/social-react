77c0d172b029a70aa06ca16d9971ebca60541409
SergSmith64 <sergsmith2008@yandex.ru>
2020-02-16 01:33:41 +0400

    feat: add two components

--- social/src/App.js
+++ social/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Tech />
     </div>
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News</a>
+      <a href="#">About</a>
+    </div>
+  )
+}
+
+const Tech = () => {
+  return (
+    <ul>
+      <li>11</li>
+      <li>22</li>
+      <li>33</li>
+      <li>44</li>
+    </ul>
+  )
 }
 
 export default App;

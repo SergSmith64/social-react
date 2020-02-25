0cc08de4000657cae5e9df572debaaed2e20944b
SergSmith64 <sergsmith2008@yandex.ru>
2020-02-25 18:08:42 +0400

    fix: grid

--- social/src/App.js
+++ social/src/App.js
@@ -13,6 +13,15 @@ const App = () => {
 			<Nav className="nav"></Nav>
 			<Main className="main"></Main>
 			<Footer className="footer"></Footer>
+			<div className="foot1">
+			The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link,
+valid, navigable address as the href value. If you cannot provide a valid href, butevcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid still need the element to resemble a link, use a button and change it with approprvalid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link,iate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/masevcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-validter/docs/rules/anchor-is-valid.md
+			</div>
+			<div className="foot2">
+			The href attribute requires a valid value to be accessible. Provide a
+valid, navigable address as the href value. If you  element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md The href attribute requires a valid value to be accessible. Provide a
+valid, navigable address as the href value.
+			</div>
 		</div>
 	);
 }

9dc2094a449a3a857f39a1f04103f2011841fbfb
SergSmith64 <sergsmith2008@yandex.ru>
2020-03-09 20:03:19 +0400

    feat: add logo in main

--- social/src/components/main.js
+++ social/src/components/main.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import logo from './logo-5ka__2.jpg';
 import './main.css';
 
 const Main = () => {
     return (
       <div>
-        <img src="https://cheapthailand.ru/images/Shapka.jpg" alt="Thai"></img>
-        <ul>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-        <li>44</li>
-      </ul>
-      <img src=".././components/logo-5ka__2.jpg" alt="Thai"></img>
+          <img src="https://cheapthailand.ru/images/Shapka.jpg" alt="Thai"></img>
+          <ul>
+            <li>11</li>
+            <li>22</li>
+            <li>33</li>
+          </ul>
+          <img src={logo} className="logo5" alt="logo" />
       </div>
     )
   }

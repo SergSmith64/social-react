92d9b4e3bd749affb9cad87e3adf350fe9f8d013
SergSmith64 <sergsmith2008@yandex.ru>
2020-03-09 16:14:46 +0400

    feat: add pic to main-section

--- social/src/components/main.js
+++ social/src/components/main.js
@@ -3,12 +3,16 @@ import './main.css';
 
 const Main = () => {
     return (
-      <ul>
+      <div>
+        <img src="https://cheapthailand.ru/images/Shapka.jpg" alt="Thai"></img>
+        <ul>
         <li>11</li>
         <li>22</li>
         <li>33</li>
         <li>44</li>
       </ul>
+      <img src=".././components/logo-5ka__2.jpg" alt="Thai"></img>
+      </div>
     )
   }
 

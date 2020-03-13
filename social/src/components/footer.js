a995d00551d7de5413fbf30df2e629aed992bc16
SergSmith64 <sergsmith2008@yandex.ru>
2020-03-13 14:23:38 +0400

    fix: footer

--- social/src/components/footer.js
+++ social/src/components/footer.js
@@ -4,7 +4,11 @@ import './footer.css';
 const Footer = () => {
     return (
       <div className="footer__wrapper">
-        это футер
+        <a href="./home">Home</a>
+        <a href="./news">News</a>
+        <a href="./money">Money</a>
+        <a href="./about">About</a>
+        <a href="./contacts">Contacts</a>
       </div>
     )
   }

bd00b611b5b96949afa508fbe6f3c0c90ec28cf0
SergSmith64 <sergsmith2008@yandex.ru>
2020-03-18 12:19:03 +0400

    feat: css->css.module

--- social/src/components/profile.js
+++ social/src/components/profile.js
@@ -5,75 +5,77 @@ import logo__3 from './ava__3.jpg';
 import logo__4 from './ava__4.jpg';
 import logo__5 from './ava__5.jpg';
 import logo__6 from './ava__6.jpg';
-import './profile.css';
+import classes from './profile.module.css';
 
 const Profile = () => {
 	return (
-		<div className="profile__wrapper">
+		// <div className="profile__wrapper">
+		<div className={classes.profile__wrapper}>
 
-			<div className="profile__item">
+			{/* <div className="profile__item"> */}
+			<div className={classes.profile__item}>
 				<h3>Это чей-то Профиль</h3>
-				<img src={logo__1} className="ava" alt="avatar" />
+				<img src={logo__1} className={classes.ava} alt="avatar" />
 				<h4>Описание профиля: интересы: музыка, кино, страны, где уже побывал</h4>
 				<div className="posts">
-					<div className="posts__item">Post 1</div>
-					<div className="posts__item">Post 2</div>
-					<div className="posts__item">Post 3</div>
+					<div className={classes.posts__item}>Post 1</div>
+					<div className={classes.posts__item}>Post 2</div>
+					<div className={classes.posts__item}>Post 3</div>
 				</div>
 			</div>
 
-			<div className="profile__item">
+			<div className={classes.profile__item}>
 				<h3>Это чей-то Профиль</h3>
-				<img src={logo__2} className="ava" alt="avatar" />
+				<img src={logo__2} className={classes.ava} alt="avatar" />
 				<h4>Описание профиля: интересы: музыка, кино, страны, где уже побывал</h4>
 				<div className="posts">
-					<div className="posts__item">Post 1</div>
-					<div className="posts__item">Post 2</div>
-					<div className="posts__item">Post 3</div>
+					<div className={classes.posts__item}>Post 1</div>
+					<div className={classes.posts__item}>Post 2</div>
+					<div className={classes.posts__item}>Post 3</div>
 				</div>
 			</div>
 
-			<div className="profile__item">
+			<div className={classes.profile__item}>
 				<h3>Это чей-то Профиль</h3>
-				<img src={logo__3} className="ava" alt="avatar" />
+				<img src={logo__3} className={classes.ava} alt="avatar" />
 				<h4>Описание профиля: интересы: музыка, кино, страны, где уже побывал</h4>
 				<div className="posts">
-					<div className="posts__item">Post 1</div>
-					<div className="posts__item">Post 2</div>
-					<div className="posts__item">Post 3</div>
+					<div className={classes.posts__item}>Post 1</div>
+					<div className={classes.posts__item}>Post 2</div>
+					<div className={classes.posts__item}>Post 3</div>
 				</div>
 			</div>
 
-			<div className="profile__item">
+			<div className={classes.profile__item}>
 				<h3>Это чей-то Профиль</h3>
-				<img src={logo__4} className="ava" alt="avatar" />
+				<img src={logo__4} className={classes.ava} alt="avatar" />
 				<h4>Описание профиля: интересы: музыка, кино, страны, где уже побывал</h4>
 				<div className="posts">
-					<div className="posts__item">Post 1</div>
-					<div className="posts__item">Post 2</div>
-					<div className="posts__item">Post 3</div>
+					<div className={classes.posts__item}>Post 1</div>
+					<div className={classes.posts__item}>Post 2</div>
+					<div className={classes.posts__item}>Post 3</div>
 				</div>
 			</div>
 
-			<div className="profile__item">
+			<div className={classes.profile__item}>
 				<h3>Это чей-то Профиль</h3>
-				<img src={logo__5} className="ava" alt="avatar" />
+				<img src={logo__5} className={classes.ava} alt="avatar" />
 				<h4>Описание профиля: интересы: музыка, кино, страны, где уже побывал</h4>
 				<div className="posts">
-					<div className="posts__item">Post 1</div>
-					<div className="posts__item">Post 2</div>
-					<div className="posts__item">Post 3</div>
+					<div className={classes.posts__item}>Post 1</div>
+					<div className={classes.posts__item}>Post 2</div>
+					<div className={classes.posts__item}>Post 3</div>
 				</div>
 			</div>
 
-			<div className="profile__item">
+			<div className={classes.profile__item}>
 				<h3>Это чей-то Профиль</h3>
-				<img src={logo__6} className="ava" alt="avatar" />
+				<img src={logo__6} className={classes.ava} alt="avatar" />
 				<h4>Описание профиля: интересы: музыка, кино, страны, где уже побывал</h4>
 				<div className="posts">
-					<div className="posts__item">Post 1</div>
-					<div className="posts__item">Post 2</div>
-					<div className="posts__item">Post 3</div>
+					<div className={classes.posts__item}>Post 1</div>
+					<div className={classes.posts__item}>Post 2</div>
+					<div className={classes.posts__item}>Post 3</div>
 				</div>
 			</div>
 

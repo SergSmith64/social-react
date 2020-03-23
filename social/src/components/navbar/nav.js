c2c6e416ad056f928a0250977a2d851bda584837
SergSmith64 <sergsmith2008@yandex.ru>
2020-03-23 18:02:23 +0400

    fix

--- social/src/components/navbar/nav.js
+++ social/src/components/navbar/nav.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './nav.module.css';
 
 const Nav = () => {
 		return (
 			<div className={classes.nav__wrapper}>
 				{/* <a href="#">Профиль</a> */}
-				<a href="/Main">Профиль</a>
+				{/* <a href="/Main">Профиль</a> */}
+				{/* <NavLink to="/Main">Профиль</NavLink> */}
+				<div>
+					<NavLink to="/Main" activeClassName={classes.activeLink}>Профиль</NavLink>
+				</div>
 
 				{/* <a href="#">Сообщения</a> */}
-				<a href="/Dialogs">Сообщения</a>
+				{/* <a href="/Dialogs">Сообщения</a> */}
+				<div>
+					<NavLink to="/Dialogs" activeClassName={classes.activeLink}>Сообщения</NavLink>
+				</div>
 				
 				{/* <a href="#">Новости</a> */}
-				<a href="/News">Сообщения</a>
+				{/* <a href="/News">Новости</a> */}
+				<div>
+					<NavLink to="/News" activeClassName={classes.activeLink}>Новости</NavLink>
+				</div>
 
-				<a href="#">Музыка</a>
-				<a href="#">НАСТРОЙКИ</a>
+				<div>
+					<a href="#">Музыка</a>
+				</div>
+
+				<div>
+					<a href="#">НАСТРОЙКИ</a>
+				</div>
 			</div>
 		)
 	}

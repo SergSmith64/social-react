bf50ef82ca6d4d99f95e12fe0406fb34158cf4c1
SergSmith64 <sergsmith2008@yandex.ru>
2020-03-23 12:34:02 +0400

    feat: add routing to 2 pages

--- social/src/App.js
+++ social/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header';
 import Nav from './components/navbar/nav';
@@ -8,29 +9,33 @@ import Footer from './components/footer/footer';
 
 const App = () => {
 	return (
-		<div className="app-wrapper">
-			<Header className="header"></Header>
-			<Nav className="nav"></Nav>
-			<Main className="main"></Main>
-			{/* <Dialogs /> */}
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header className="header"></Header>
+				<Nav className="nav"></Nav>
+				<Route path='/main' component={Main} />
+				<Route path='/dialogs' component={Dialogs} />
+				{/* <Main className="main"></Main> */}
+				{/* <Dialogs /> */}
 
-			<div className="advert1">
-				<p>Здесь могла бы быть ваша реклама</p>
-				<p>Здесь могла бы быть ваша реклама</p>
-				<p>Здесь могла бы быть ваша реклама</p>
-			</div>
-			<div className="advert2">
-				<p>НИЗ ПОСЕРЕДИНЕ С ОТБИВКОЙ ВПРАВО:</p>
-				<p>The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value.</p>
-			</div>
-			<div className="advert3">
-				<p>ЭТО ПРАВЫЙ НИЖНИЙ УГОЛ</p>
-				<p>ЭТО ПРАВЫЙ НИЖНИЙ УГОЛ</p>
-				<p>ЭТО ПРАВЫЙ НИЖНИЙ УГОЛ</p>
-			</div>
+				<div className="advert1">
+					<p>Здесь могла бы быть ваша реклама</p>
+					<p>Здесь могла бы быть ваша реклама</p>
+					<p>Здесь могла бы быть ваша реклама</p>
+				</div>
+				<div className="advert2">
+					<p>НИЗ ПОСЕРЕДИНЕ С ОТБИВКОЙ ВПРАВО:</p>
+					<p>The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value.</p>
+				</div>
+				<div className="advert3">
+					<p>ЭТО ПРАВЫЙ НИЖНИЙ УГОЛ</p>
+					<p>ЭТО ПРАВЫЙ НИЖНИЙ УГОЛ</p>
+					<p>ЭТО ПРАВЫЙ НИЖНИЙ УГОЛ</p>
+				</div>
 
-			<Footer className="footer"></Footer>
-		</div>
+				<Footer className="footer"></Footer>
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- social/src/components/navbar/nav.js
+++ social/src/components/navbar/nav.js
@@ -2,17 +2,19 @@ import React from 'react';
 import classes from './nav.module.css';
 
 const Nav = () => {
-    return (
-      <div className={classes.nav__wrapper}>
-        {/* <a href="#">Профиль</a> */}
-        <a href="/Main">Профиль</a>
-        {/* <a href="#">Сообщения</a> */}
-        <a href="/Dialogs">Сообщения</a>
-        <a href="#">Новости</a>
-        <a href="#">Музыка</a>
-        <a href="#">НАСТРОЙКИ</a>
-      </div>
-    )
-  }
+		return (
+			<div className={classes.nav__wrapper}>
+				{/* <a href="#">Профиль</a> */}
+				<a href="/Main">Профиль</a>
 
-  export default Nav;
+				{/* <a href="#">Сообщения</a> */}
+				<a href="/Dialogs">Сообщения</a>
+				
+				<a href="#">Новости</a>
+				<a href="#">Музыка</a>
+				<a href="#">НАСТРОЙКИ</a>
+			</div>
+		)
+	}
+
+	export default Nav;
